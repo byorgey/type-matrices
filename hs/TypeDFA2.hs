3e3f314fd25e3288634a49e9ae0bd9baf3507b98
Brent Yorgey <byorgey@cis.upenn.edu>
2013-07-06 12:52:19 -0400

    playing around with existential wrappers, helper functions, etc.

--- hs/TypeDFA2.hs
+++ hs/TypeDFA2.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE StandaloneDeriving #-}
@@ -10,16 +8,7 @@
 {-# LANGUAGE DataKinds #-}
 
 import Data.Void
-import GHC.Exts
 import GHC.TypeLits
-
-{-
-
-Still to do:
-
-+ Generalize using a representation of matrices at the type level?
-
--}
 
 ----------------------------------------------------------------------
 -- Initial binary tree type
@@ -36,27 +25,23 @@ treeFold f g = go
 ----------------------------------------------------------------------
 -- Derived, restricted type, for the regex (ab)*
 
-data Tree' :: Nat -> Nat -> [*] -> * where
-  Leaf' :: Transition i j ts -> Tree' i j ts
-  Fork' :: Tree' i k ts -> Tree' k j ts -> Tree' i j ts
+data Tree' :: Nat -> Nat -> * -> * -> * where
+  Leaf' :: Transition i j a b -> Tree' i j a b
+  Fork' :: Tree' i k a b -> Tree' k j a b -> Tree' i j a b
 
-type family All (c :: * -> Constraint) (ts :: [*]) :: Constraint
-type instance All c '[]       = ()
-type instance All c (t ': ts) = (c t, All c ts)
+deriving instance (Show a, Show b) => Show (Tree' i j a b)
 
-deriving instance (All Show ts) => Show (Tree' i j ts)
-
-tree'Fold :: (forall i j. Transition i j ts -> r) -> (r -> r -> r) -> Tree' i j ts -> r
+tree'Fold :: (forall i j. Transition i j a b -> r) -> (r -> r -> r) -> Tree' i j a b -> r
 tree'Fold f _ (Leaf' a)   = f a
 tree'Fold f g (Fork' l r) = g (tree'Fold f g l) (tree'Fold f g r)
 
 -- could generalize this more, along the lines of what was in some of
 -- the blog posts
-data Transition :: Nat -> Nat -> [*] -> * where
-  T01 :: a -> Transition 0 1 [a,b]
-  T10 :: b -> Transition 1 0 [a,b]
+data Transition :: Nat -> Nat -> * -> * -> * where
+  T01 :: a -> Transition 0 1 a b
+  T10 :: b -> Transition 1 0 a b
 
-deriving instance (All Show ts) => Show (Transition i j ts)
+deriving instance (Show a, Show b) => Show (Transition i j a b)
 
 {- Having Transition be a GADT instead of a type family is important.
    Here is what happened when it was a type family:
@@ -79,47 +64,46 @@ deriving instance (All Show ts) => Show (Transition i j ts)
 ----------------------------------------------------------------------
 -- Existentially wrapping Tree' to hide the DFA state indices
 
-data Ex :: (Nat -> Nat -> [*] -> *) -> [*] -> * where
-  ExError :: Ex f ts
-  Ex      :: Sing (i :: Nat) -> Sing (j :: Nat) -> f i j ts -> Ex f ts
+data ExTree :: * -> * -> * where
+  ExTreeError :: ExTree a b
+  ExTree      :: Sing (i :: Nat) -> Sing (j :: Nat) -> Tree' i j a b -> ExTree a b
     -- The singleton arguments allow us to do case analysis on the
     -- type indices i and j at runtime.
 
-instance (Show (f i j ts)) => Show (Ex f ts) where
-  show (ExError)  = "ExError"
-  show (Ex _ _ t) = show t
+instance (Show a, Show b) => Show (ExTree a b) where
+  show (ExTreeError)  = "ExTreeError"
+  show (ExTree _ _ t) = show t
 
 ----------------------------------------------------------------------
 -- Automatically building type-correct trees with dynamic checks
 
-leafA :: a -> Ex Tree' [a,b]
-leafA a = Ex sing sing (Leaf' (T01 a))
+leafA :: a -> ExTree a b
+leafA a = ExTree sing sing (Leaf' (T01 a))
 
-leafB :: b -> Ex Tree' [a,b]
-leafB b = Ex sing sing (Leaf' (T10 b))
+leafB :: b -> ExTree a b
+leafB b = ExTree sing sing (Leaf' (T10 b))
 
 -- fork does runtime case analysis of the existential DFA state
 -- indices.  Calling eqNat checks whether they are the same, and if
 -- so, actually makes that fact known to the type checker.
-fork :: Ex Tree' [a,b] -> Ex Tree' [a,b] -> Ex Tree' [a,b]
-fork (Ex i k1 t1) (Ex k2 j t2)
+fork :: ExTree a b -> ExTree a b -> ExTree a b
+fork (ExTree i k1 t1) (ExTree k2 j t2)
   = case eqNat k1 k2 of
-      Nothing   -> ExError
-      Just Refl -> Ex i j (Fork' t1 t2)
-fork _ _ = ExError
+      Nothing   -> ExTreeError
+      Just Refl -> ExTree i j (Fork' t1 t2)
+fork _ _ = ExTreeError
 
 ----------------------------------------------------------------------
 -- Converting to and from restricted trees
 
-toTree :: Ex Tree' [a,b] -> Maybe (Tree (Either a b))
-toTree ExError    = Nothing
-toTree (Ex _ _ t) = Just $ tree'Fold leafToEither Fork t
+toTree :: ExTree a b -> Maybe (Tree (Either a b))
+toTree ExTreeError    = Nothing
+toTree (ExTree _ _ t) = Just $ tree'Fold leafToEither Fork t
   where
-    leafToEither :: Transition i j [a,b] -> Tree (Either a b)
     leafToEither (T01 a) = Leaf (Left a)
     leafToEither (T10 b) = Leaf (Right b)
 
-fromTree :: Tree (Either a b) -> Ex Tree' [a,b]
+fromTree :: Tree (Either a b) -> ExTree a b
 fromTree = treeFold (either leafA leafB) fork
 
 -- Dynamically check whether a tree conforms to (ab)*
@@ -129,7 +113,7 @@ checkTree = toTree . fromTree
 ----------------------------------------------------------------------
 -- This stuff is probably already in a library somewhere
 
-data a :=: b where
+data (:=:) (a :: Nat) (b :: Nat) where
   Refl :: a :=: a
 
 eqNat :: Sing (i :: Nat) -> Sing (j :: Nat) -> Maybe (i :=: j)
