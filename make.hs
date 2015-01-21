a3d76ee14b3b79192a20228017852a47c739d3ab
Brent Yorgey <byorgey@gmail.com>
2015-01-20 22:01:54 -0500

    switch from rubber (no longer maintained) to latexmk

--- make.hs
+++ make.hs
@@ -1,10 +1,10 @@
 import           Development.Shake
 import           Development.Shake.FilePath
 
-lhs2TeX, pdflatex, rubber :: String
+lhs2TeX, pdflatex, latexmk :: String
 lhs2TeX  = "lhs2TeX"
 pdflatex = "pdflatex"
-rubber   = "rubber"
+latexmk  = "latexmk"
 
 main :: IO ()
 main = shake shakeOptions $ do
@@ -16,13 +16,8 @@ main = shake shakeOptions $ do
         need [input]
         cmd lhs2TeX $ ["-o", output] ++ [input]
 
-    -- In general, running LaTeX enough times to resolve everything
-    -- requires parsing the output of LaTeX.  A nice tool that can do
-    -- this automatically is "rubber".  If you have (or can install)
-    -- rubber then this ought to work.  Otherwise we'll have to go
-    -- with the tried and true "just run LaTeX twice no matter what".
     "*.pdf" *> \output -> do
         let input = replaceExtension output "tex"
         need [input]
         () <- cmd pdflatex $ ["--enable-write18", input]
-        cmd rubber $ ["-f", "-d", input]
+        cmd latexmk $ ["-pdf", input]
