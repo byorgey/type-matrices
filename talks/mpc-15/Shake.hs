8234f622aabea03523016921737cbeffeac35705
Brent Yorgey <byorgey@gmail.com>
2015-06-02 09:47:10 -0400

    start slides for MPC 2015

---
+++ talks/mpc-15/Shake.hs
@@ -0,0 +1,18 @@
+import           Development.Shake
+import           Development.Shake.FilePath
+
+lhs2TeX, pdflatex, mklatex :: String
+lhs2TeX  = "lhs2TeX"
+pdflatex = "pdflatex"
+mklatex  = "mklatex"
+
+main :: IO ()
+main = shake shakeOptions $ do
+
+    want ["type-matrices-mpc-15.pdf"]
+
+    "*.pdf" *> \output -> do
+        let input = replaceExtension output "tex"
+        hsFiles <- getDirectoryFiles "" ["*.hs"]
+        need (input : hsFiles)
+        cmd pdflatex $ ["--enable-write18", input]
